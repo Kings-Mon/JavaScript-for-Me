aff95e31ad5ec146a36c5dde97404169ff81e27e
Kingshuk Mondal <141356181+Kings-Mon@users.noreply.github.com>
2024-11-09 22:45:52 +0530

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,81 @@
+// Primitive
+
+// 7 types :- String, Number, Boolean. Null, Undefined, Symbol, BigInt
+
+const score = 1000
+const scoreValue = 1000.6
+
+const isLoggedIn = false
+const outsidetemp = null
+let unerEmail;
+
+const ID = Symbol('321')
+const anotherID = Symbol('321')
+
+console.log( ID === anotherID);
+
+const bigNumber = 16546516546168362989n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", "Krish", "Chitthi"]
+
+let muObj = {
+    name: "Kingshuk",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("HELLO WORLD!");
+}
+
+console.log(typeof bigNumber);  // bigint
+console.log(typeof outsidetemp); // object (this is because `null` is treated as an object)
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof myFunction); // function
+console.log(typeof anotherID);  // symbol
+console.log(typeof heros);      // object (because arrays are technically objects)
+
+
+// https://tc39.es/ecma262/#sec-typeof-operator
+
+// ==================================================================== //
+// Return type of variables in JavaScript :- 
+
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
+// ============================================================== //
+
+// Stack (Primitive), Heap (Non-Primitive)
+// Stack gives you a copy, but Heap gives you a reference
+
+
+let myInstaHandle = 'different_perspetive_007'
+
+let GithubName = myInstaHandle
+GithubName = "Kings_Mon"
+
+console.log(myInstaHandle);
+console.log(GithubName);
+
+let userOne = {
+    email: "kingshuk@gmail.com",
+    upi: "king123@ydl"
+}
+
+console.log(userOne.email);
