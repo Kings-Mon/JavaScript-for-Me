5a1b9085eb6d051dd62ba5aa22dcdcdf82df001a
Kingshuk Mondal <141356181+Kings-Mon@users.noreply.github.com>
2024-11-09 21:45:16 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // Enabling strict mode for better error checking
+
+// alert(9 + 9); // if you're running in a browser
+// In Node.js, console.log() is used
+
+console.log(9
+    +6  // readibility should be high
+)
+console.log("Kingshuk")
+
+let name = "kingshuk"
+let age = 21
+let isLoggedIn = false
+
+
+// JavaScript Data Types :-
+// Number => in the range of 2 to the power of 53
+// BigInt => Larger integers than Number type
+// String => Text enclosed in "" or ''
+// Boolean => true or false
+// null => A standalone value representing no value or empty object reference
+// undefined => Variable declared but not assigned a value
+// Symbol => Unique and immutable value
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
