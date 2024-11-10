b8fe82bcb2b2e744a2a1bb5de7e143d7f38deb3f
Kingshuk Mondal <141356181+Kings-Mon@users.noreply.github.com>
2024-11-10 11:18:03 +0530

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch - case 
+
+// switch (key) {
+//     case value:
+//         // execute this block if key === value
+//         break;
+//     default:
+//         // execute this block if no case matches
+//         break;
+// }
+
+const month = 2 // "Feb"
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("Default Case Match!");
+        break;
+}
